20c846dfd4a9001a97b0206d988f3628e0421ffe
Nolan Knill <studentprogrammer@Admins-Mac-mini.local>
2012-12-19 16:57:42 -0500

    Begun day 3 challenge

--- day3.py
+++ day3.py
@@ -1,3 +1,21 @@
 """Welcome to cipher day!
 write a program that can encrypt texts with an alphabetical caesar cipher. This cipher can ignore numbers, symbols, and whitespace.
 for extra credit, add a "decrypt" function to your program!"""
+
+letter_values = ['a'=>1, 'b'=>2, 'c'=>3, 'd'=4, 'e'=>5, 'f'=>6, 'g'=>7]
+
+def shift_letters(amount):
+	shift_value = amount
+	
+	
+def encrypt(string):
+	
+	return encypted_string
+		
+def decrypt(string):
+	
+	
+
+test = "Hey there how is your day going?"
+encrypted_test = encrypt(test)
+print "This is your string encrypted: %s" % encrypted_test
