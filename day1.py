7af0430e26c40b45ecd4ae94a60ce93e32ce2246
Nolan Knill <studentprogrammer@Admins-Mac-mini.local>
2012-12-19 14:21:54 -0500

    Adding rest of files

---
+++ day1.py
@@ -0,0 +1,15 @@
+"""
+create a program that will ask the users name, age, and reddit username. have it tell them the information back, in the format:
+your name is (blank), you are (blank) years old, and your username is (blank)
+for extra credit, have the program log this information in a file to be accessed later.
+"""
+name = raw_input("Enter your name: ")
+age = raw_input("Enter your age: ")
+username = raw_input("Enter your reddit username: ")
+save_string = "your name is %s, you are %s years old, and your username is %s." %(name, age, username)
+
+print save_string
+
+save_file = open('txt/day1.txt', 'a')
+save_file.write(save_string)
+save_file.close()
