8bbee65dda70402ae7089baa682ee606e80ebb8d
unknown <NolansMac@.(none)>
2012-12-19 21:40:55 -0500

    day 2 is finished

--- python/day2.py
+++ python/day2.py
@@ -7,4 +7,26 @@ EXTRA CREDIT: make the calculator have multiple functions! Not only should it be
 calculate F = M * A, but also A = F/M, and M = F/A!
 """
 
- 
+while True:
+    solve_for = input("What do you want to solve for? (F, M, or A)").upper()
+    
+    if (solve_for == "F"):
+        print ("\nF = M * A")
+        mass = input("Enter Mass: ")
+        acceleration = input("Enter Acceleration: ")
+        force = float(mass)*float(acceleration)
+        print("Force = ", force)
+        
+    elif(solve_for == "M"):
+        print ("\nM = F / A")
+        force = input("Enter Force: ")
+        acceleration = input("Enter Acceleration: ")
+        mass = float(force)/float(acceleration)
+        print("Mass = ", mass)
+    
+    elif (solve_for == "A"):
+        print ("\nA = F / M")
+        force= input("Enter Force: ")
+        mass = input("Enter Mass: ")
+        acceleration = float(force) / float(mass)
+        print("Acceleration = ", acceleration)
