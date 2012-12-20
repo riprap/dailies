d21b20cf047d6e693f3cfe851908e0ff771a345b
unknown <NolansMac@.(none)>
2012-12-19 21:06:02 -0500

    Moved txt folder, this is to work with python 3.2

--- python/day1.py
+++ python/day1.py
@@ -3,12 +3,12 @@ create a program that will ask the users name, age, and reddit username. have it
 your name is (blank), you are (blank) years old, and your username is (blank)
 for extra credit, have the program log this information in a file to be accessed later.
 """
-name = raw_input("Enter your name: ")
-age = raw_input("Enter your age: ")
-username = raw_input("Enter your reddit username: ")
+name =input("Enter your name: ")
+age = input("Enter your age: ")
+username = input("Enter your reddit username: ")
 save_string = "your name is %s, you are %s years old, and your username is %s." %(name, age, username)
 
-print save_string
+print(save_string)
 
 save_file = open('txt/day1.txt', 'a')
 save_file.write(save_string)
