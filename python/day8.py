29fe142be39b84c97291a62f756aaca145fff481
Nolan Knill <studentprogrammer@Admins-Mac-mini.local>
2012-12-20 15:03:07 -0500

    Finished base challenge

--- python/day8.py
+++ python/day8.py
@@ -2,3 +2,18 @@
 write a program that will print the song "99 bottles of beer on the wall".
 for extra credit, do not allow the program to print each loop on a new line.
 """
+
+bottles = 99
+
+while bottles > 0:
+	
+	if bottles != 0:
+		print "%i bottles of beer on the wall, %i bottles of beer." %(bottles, bottles)
+		bottles-=1
+		
+		if bottles == 0:
+			print "Take one down and pass it around, no more bottles of beer on the wall."
+			print "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
+	
+		else:
+			print "Take one down and pass it around, %i bottles of beer on the wall." % bottles
