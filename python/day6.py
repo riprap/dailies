eceff7c0d8e3a72d04dcc54f37b7a5d019e52381
riprap <nolanknill@rogers.com>
2013-01-02 23:53:47 -0500

    Remove duplicate files

--- python/day6.py
+++
@@ -1,4 +0,0 @@
-"""
-You're challenge for today is to create a program that can calculate pi accurately to at least 30 decimal places.
-Try not to cheat :)
-"""
--- python/day7.py
+++
@@ -1,8 +0,0 @@
-""" 
-Write a program that can translate Morse code in the format of ...---...
-A space and a slash will be placed between words. ..- / --.-
-For bonus, add the capability of going from a string to Morse code.
-Super-bonus if your program can flash or beep the Morse.
-This is your Morse to translate:
-.... . .-.. .-.. --- / -.. .- .. .-.. -.-- / .--. .-. --- --. .-. .- -- -- . .-. / --. --- --- -.. / .-.. ..- -.-. -.- / --- -. / - .... . / -.-. .... .- .-.. .-.. . -. --. . ... / - --- -.. .- -.--
-"""
